074d3e9376abf158930f5b88f40c1f7855364640
Alexey Ershov <alexa3mail@gmail.com>
2024-09-21 16:36:27 +0300

    minor changes

--- nixos/modules/containers/watchtower/default.nix
+++ nixos/modules/containers/watchtower/default.nix
@@ -1,6 +1,9 @@
 {...}: {
   virtualisation.oci-containers.containers."watchtower" = {
     image = "quay.io/containrrr/watchtower:stable";
+    extraOptions = [
+      "--label-enable"
+    ];
     volumes = [
       "/var/run/docker.sock:/var/run/docker.sock"
     ];
