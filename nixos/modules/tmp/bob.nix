53709a0c75069121749115bd36bfc14711130fff
Alexey Ershov <alexa3mail@gmail.com>
2024-09-24 21:47:50 +0300

    last fix

--- nixos/modules/tmp/bob.nix
+++ nixos/modules/tmp/bob.nix
@@ -3,7 +3,7 @@
     addSSL = true;
     enableACME = true;
     locations."/" = {
-      proxyPass = "http://127.0.0.1:8000";
+      proxyPass = "http://bob:8000";
       extraConfig = ''
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Host $host;
