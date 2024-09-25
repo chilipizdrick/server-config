0d12a2dad847e93869b5c73694cbb7f2e51ff92a
Alexey Ershov <alexa3mail@gmail.com>
2024-09-25 13:39:07 +0300

    fix nginx headers

--- nixos/modules/tmp/bob.nix
+++ nixos/modules/tmp/bob.nix
@@ -4,11 +4,11 @@
     enableACME = true;
     locations."/" = {
       proxyPass = "http://localhost:8000";
-      extraConfig = ''
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $host;
-        proxy_redirect off;
-      '';
+      # extraConfig = ''
+      #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      #   proxy_set_header Host $host;
+      #   proxy_redirect off;
+      # '';
     };
   };
 }
