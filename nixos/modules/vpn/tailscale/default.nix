39a7b09438fa469d467e846c1370e2a818b566f2
Alexey Ershov <alexa3mail@gmail.com>
2024-10-10 21:48:57 +0300

    edit tailscale

--- nixos/modules/vpn/tailscale/default.nix
+++ nixos/modules/vpn/tailscale/default.nix
@@ -5,27 +5,30 @@
     openFirewall = true;
     port = 41641;
     interfaceName = "tailscale0";
+    extraUpFlags = [
+      "--advertise-exit-node"
+    ];
   };
 
   environment.systemPackages = [pkgs.tailscale];
 
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-    after = ["network-pre.target" "tailscale.service"];
-    wants = ["network-pre.target" "tailscale.service"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig.Type = "oneshot";
-    script = with pkgs; ''
-      sleep 2
-
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      ${tailscale}/bin/tailscale up -authkey tskey-examplekeyhere
-    '';
-  };
+  # systemd.services.tailscale-autoconnect = {
+  #   description = "Automatic connection to Tailscale";
+  #   after = ["network-pre.target" "tailscale.service"];
+  #   wants = ["network-pre.target" "tailscale.service"];
+  #   wantedBy = ["multi-user.target"];
+  #   serviceConfig.Type = "oneshot";
+  #   script = with pkgs; ''
+  #     sleep 2
+  #
+  #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+  #     if [ $status = "Running" ]; then # if so, then do nothing
+  #       exit 0
+  #     fi
+  #
+  #     ${tailscale}/bin/tailscale up -authkey tskey-examplekeyhere
+  #   '';
+  # };
 
   networking.firewall = {
     trustedInterfaces = ["tailscale0"];
