18919a91b486512d1d9f12abc4d9d08d62d3f500
Alexey Ershov <alexa3mail@gmail.com>
2024-10-13 12:13:56 +0300

    tailscale

--- nixos/modules/vpn/tailscale/default.nix
+++ nixos/modules/vpn/tailscale/default.nix
@@ -12,24 +12,6 @@
 
   environment.systemPackages = [pkgs.tailscale];
 
-  # systemd.services.tailscale-autoconnect = {
-  #   description = "Automatic connection to Tailscale";
-  #   after = ["network-pre.target" "tailscale.service"];
-  #   wants = ["network-pre.target" "tailscale.service"];
-  #   wantedBy = ["multi-user.target"];
-  #   serviceConfig.Type = "oneshot";
-  #   script = with pkgs; ''
-  #     sleep 2
-  #
-  #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-  #     if [ $status = "Running" ]; then # if so, then do nothing
-  #       exit 0
-  #     fi
-  #
-  #     ${tailscale}/bin/tailscale up -authkey tskey-examplekeyhere
-  #   '';
-  # };
-
   networking.firewall = {
     trustedInterfaces = ["tailscale0"];
   };
